d3d2417ba2bfc3818a8921010748954d51081f41
Dustin Winkler <dustywinkler@gmail.com>
2021-11-15 19:40:54 -0800

    Fixed Issue #1

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -1,21 +1,24 @@
 export const addMessageToStore = (state, payload) => {
   const { message, sender } = payload;
+  console.log(message, sender);
   // if sender isn't null, that means the message needs to be put in a brand new convo
   if (sender !== null) {
     const newConvo = {
       id: message.conversationId,
       otherUser: sender,
       messages: [message],
+      latestMessageText: message.text
     };
-    newConvo.latestMessageText = message.text;
     return [newConvo, ...state];
   }
 
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+      return {
+        ...convo,
+        messages: [...convo.messages, message],
+        latestMessageText: message.text
+      }
     } else {
       return convo;
     }
@@ -69,10 +72,12 @@ export const addSearchedUsersToStore = (state, users) => {
 export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
     if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+      return {
+        ...convo,
+        id: message.conversationId,
+        messages: [...convo.messages, message],
+        latestMessageText: message.text
+      }
     } else {
       return convo;
     }
--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -71,7 +71,16 @@ export const logout = (id) => async (dispatch) => {
 
 export const fetchConversations = () => async (dispatch) => {
   try {
-    const { data } = await axios.get("/api/conversations");
+    let { data } = await axios.get("/api/conversations");
+    data = data.map(convo => {
+      return {
+        ...convo,
+        messages: convo.messages.sort((message1, message2) => {
+          return new Date(message1.createdAt) - new Date(message2.createdAt)
+        })
+      }
+    })
+    console.log('wow', data);
     dispatch(gotConversations(data));
   } catch (error) {
     console.error(error);
@@ -93,10 +102,9 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = (body) => async (dispatch) => {
   try {
-    const data = saveMessage(body);
-
+    const data = await saveMessage(body);
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
     } else {
